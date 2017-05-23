abda17cb6dd265e212cb678051e05ad33c99dfc3
Hongkee Yoon <bluehope@gmail.com>
2017-05-24 00:04:15 +0900

    * reflect changes from ( Hamiltonian_info_type  Basis transformation )

--- src/Spin_Exchange.jl
+++ src/Spin_Exchange.jl
@@ -3,7 +3,7 @@ import DFTforge
 using DFTforge.DFTrefinery
 using DFTcommon
 import MAT
-X_VERSION = VersionNumber("0.2.0-dev+20170508");
+X_VERSION = VersionNumber("0.3.0-dev+20170518");
 println(" X_VERSION: ",X_VERSION)
 
 @everywhere import DFTforge
@@ -45,6 +45,7 @@ arg_input = parse_input(ARGS,arg_input)
 ## 1.3 Set values from intput (arg_input)
 DFT_type = arg_input.DFT_type
 Wannier90_type = arg_input.Wannier90_type
+spin_type = arg_input.spin_type
 
 result_file = arg_input.result_file
 ChemP_delta_ev = arg_input.ChemP_delta_ev
@@ -72,13 +73,22 @@ if ((DFTcommon.unmask == orbital_mask_option) || (DFTcommon.mask == orbital_mask
  orbital_mask_on = true
 end
 
+# orbital orbital_reassign
+basisTransform_rule = basisTransform_rule_type()
+if haskey(arg_input.Optional,"basisTransform")
+  basisTransform_rule = arg_input.Optional["basisTransform"]
+end
+
 println(DFTcommon.bar_string) # print ====...====
 println(atom12_list)
+println("q_point_num ",q_point_num, "\tk_point_num ",k_point_num)
 println(string("DFT_type ",DFT_type))
+println(string("orbital_mask_option ",orbital_mask_option))
 println("mask1list ",orbital_mask1_list,"\tmask2list ",orbital_mask2_list)
+println("basisTransform", basisTransform_rule)
 
 ## 1.4 Set caluations type and ouput folder
-cal_type = "jq.spin" # xq, ...
+cal_type = "jq.spin.test" # xq, ...
 
 if (DFTcommon.Wannier90 == DFT_type)
   cal_type = string(cal_type,".wannier")
@@ -105,19 +115,20 @@ println(DFTcommon.bar_string) # print ====...====
 ##############################################################################
 
 ## 2.1 Set Input info
-scf_r = [];
+hamiltonian_info = [];
 if (DFTcommon.OpenMX == DFT_type)
-  scf_r = set_current_dftdataset(result_file, DFTcommon.OpenMX, DFTcommon.colinear_type)
+  hamiltonian_info = set_current_dftdataset(result_file, DFTcommon.OpenMX, spin_type,basisTransform_rule)
 elseif (DFTcommon.Wannier90 == DFT_type)
   atomnum = arg_input.Wannier_Optional_Info.atomnum
   atompos = arg_input.Wannier_Optional_Info.atompos
   atoms_orbitals_list = arg_input.Wannier_Optional_Info.atoms_orbitals_list
 
-  scf_r = DFTforge.read_dftresult(result_file,DFT_type,Wannier90_type,atoms_orbitals_list,atomnum,atompos)
-  scf_r = set_current_dftdataset(scf_r, DFT_type, DFTcommon.colinear_type)
+  #hamiltonian_info = DFTforge.read_dftresult(result_file,DFT_type,Wannier90_type,atoms_orbitals_list,atomnum,atompos,basisTransform_rule)
+  hamiltonian_info = set_current_dftdataset(result_file,DFT_type,Wannier90_type,spin_type,atoms_orbitals_list,atomnum,atompos,basisTransform_rule)
+  #hamiltonian_info = set_current_dftdataset(scf_r, DFT_type, spin_type,basisTransform_rule)
 end
 
-DFTforge.pwork(set_current_dftdataset,(scf_r, DFT_type, DFTcommon.colinear_type,1));
+DFTforge.pwork(set_current_dftdataset,(hamiltonian_info, 1));
 
 ## 2.2 Generate k,q points
 k_point_list = kPoint_gen_GammaCenter(k_point_num);
@@ -184,7 +195,7 @@ end
   spin_type::DFTforge.SPINtype = input.spin_type;
 
   atom12_list::Vector{Tuple{Int64,Int64}} = input.atom12_list;
-  result_mat = zeros(Complex_my,1,length(atom12_list))
+  result_mat = zeros(Complex_my,4,length(atom12_list))
   #atom1::Int = input.atom1;
   #atom2::Int = input.atom2;
 
@@ -202,96 +213,179 @@ end
   E_temp = get_dftdataset(result_index).scf_r.E_Temp;
 
   # Get EigenState
-  #eigenstate_k_up::Kpoint_eigenstate  = cacheread_eigenstate(k_point,1,cache_index)
-  eigenstate_kq_up::Kpoint_eigenstate = cacheread_eigenstate(kq_point,1,cache_index)
-  eigenstate_k_down::Kpoint_eigenstate  = cacheread_eigenstate(k_point,2,cache_index)
-  #eigenstate_kq_down::Kpoint_eigenstate = cacheread_eigenstate(kq_point,2,cache_index)
+  eigenstate_k_up::Kpoint_eigenstate_only  = cacheread_eigenstate(k_point,1,cache_index)
+  eigenstate_kq_up::Kpoint_eigenstate_only = cacheread_eigenstate(kq_point,1,cache_index)
+  eigenstate_k_down::Kpoint_eigenstate_only  = cacheread_eigenstate(k_point,2,cache_index)
+  eigenstate_kq_down::Kpoint_eigenstate_only = cacheread_eigenstate(kq_point,2,cache_index)
 
   # Get Hamiltonian
-  Hks_up::Hamiltonian_type = cacheread_Hamiltonian(1,cache_index)
-  Hks_down::Hamiltonian_type = cacheread_Hamiltonian(2,cache_index)
-  (orbitalStartIdx,orbitalNums) = cacheread_atomsOrbital_lists(cache_index)
+  #Hks_kq_up::Hamiltonian_type  = cacheread_Hamiltonian((0.0,0.0,0.0),1,cache_index)
+  #Hks_k_down::Hamiltonian_type = cacheread_Hamiltonian((0.0,0.0,0.0),2,cache_index)
 
-  #En_k_up::Array{Float_my,1} = eigenstate_k_up.Eigenvalues;
+  Hks_G_up::Hamiltonian_type  = cacheread_Hamiltonian((0.0,0.0,0.0),1,cache_index)
+  #Hks_G_up::Hamiltonian_type  = cacheread_Hamiltonian((0.0,0.0,0.0),1,cache_index)
+  Hks_G_down::Hamiltonian_type = cacheread_Hamiltonian((0.0,0.0,0.0),2,cache_index)
+  #Hks_G_down::Hamiltonian_type = cacheread_Hamiltonian((0.0,0.0,0.0),2,cache_index)
+
+  q_point = (kq_point[1]-k_point[1],kq_point[2]-k_point[2],kq_point[3]-k_point[3])
+  q_n_point = (-kq_point[1]+k_point[1],-kq_point[2]+k_point[2],-kq_point[3]+k_point[3])
+
+  Hks_q_up::Hamiltonian_type  = cacheread_Hamiltonian(q_point,1,cache_index)
+  Hks_q_down::Hamiltonian_type = cacheread_Hamiltonian(q_point,2,cache_index)
+
+  Hks_q_n_up::Hamiltonian_type  = cacheread_Hamiltonian(q_point,1,cache_index)
+  Hks_q_n_down::Hamiltonian_type = cacheread_Hamiltonian(q_point,2,cache_index)
+
+# #=
+  Hks_k_up::Hamiltonian_type  = cacheread_Hamiltonian(k_point,1,cache_index)
+  Hks_kq_up::Hamiltonian_type  = cacheread_Hamiltonian(kq_point,1,cache_index)
+  Hks_k_down::Hamiltonian_type = cacheread_Hamiltonian(k_point,2,cache_index)
+  Hks_kq_down::Hamiltonian_type = cacheread_Hamiltonian(kq_point,2,cache_index)
+# =#
+  (orbitalStartIdx_list,orbitalNums) = cacheread_atomsOrbital_lists(cache_index)
+
+  En_k_up::Array{Float_my,1} = eigenstate_k_up.Eigenvalues;
   Em_kq_up::Array{Float_my,1} = eigenstate_kq_up.Eigenvalues;
   En_k_down::Array{Float_my,1} = eigenstate_k_down.Eigenvalues;
-  #Em_kq_down::Array{Float_my,1} = eigenstate_kq_down.Eigenvalues;
+  Em_kq_down::Array{Float_my,1} = eigenstate_kq_down.Eigenvalues;
 
-  #Es_n_k_up::Array{Complex_my,2} = eigenstate_k_up.Eigenstate;
+  Es_n_k_up::Array{Complex_my,2} = eigenstate_k_up.Eigenstate;
   Es_m_kq_up::Array{Complex_my,2} = eigenstate_kq_up.Eigenstate;
   Es_n_k_down::Array{Complex_my,2} = eigenstate_k_down.Eigenstate;
-  #Es_m_kq_down::Array{Complex_my,2} = eigenstate_kq_down.Eigenstate;
+  Es_m_kq_down::Array{Complex_my,2} = eigenstate_kq_down.Eigenstate;
 
   for (atom12_i,atom12) in enumerate(atom12_list)
     atom1 = atom12[1]
     atom2 = atom12[2]
 
-    atom1_orbitals = orbitalStartIdx[atom1]+(1:orbitalNums[atom1]);
-    atom2_orbitals = orbitalStartIdx[atom2]+(1:orbitalNums[atom2]);
+    atom1_orbitals = orbitalStartIdx_list[atom1]+(1:orbitalNums[atom1]);
+    atom2_orbitals = orbitalStartIdx_list[atom2]+(1:orbitalNums[atom2]);
     ############################################################################
     ## Accessing Data End
     ############################################################################
 
     # mask oribtals
+    Es_n_k_up_atom1 = copy(Es_n_k_up);
+    Es_n_k_up_atom2 = copy(Es_n_k_up);
+
     Es_n_k_down_atom1 = copy(Es_n_k_down);
+    Es_n_k_down_atom2 = copy(Es_n_k_down);
+
     Es_m_kq_up_atom1  = copy(Es_m_kq_up);
     Es_m_kq_up_atom2  = copy(Es_m_kq_up);
-    Es_n_k_down_atom2 = copy(Es_n_k_down);
+
+    Es_m_kq_down_atom1 = copy(Es_m_kq_down);
+    Es_m_kq_down_atom2 = copy(Es_m_kq_down);
     if (length(orbital_mask1)>0)
       orbital_mask1_tmp = collect(1:orbitalNums[atom1]);
       for orbit1 in orbital_mask1
           deleteat!(orbital_mask1_tmp, find(orbital_mask1_tmp.==orbit1))
       end
-      Es_n_k_down_atom1[orbitalStartIdx[atom1]+orbital_mask1_tmp,:]=0.0;
+      Es_n_k_up_atom1[orbitalStartIdx_list[atom1]+orbital_mask1_tmp,:] = 0.0;
+      Es_n_k_down_atom1[orbitalStartIdx_list[atom1]+orbital_mask1_tmp,:] = 0.0;
     end
     if (length(orbital_mask2)>0)
       orbital_mask2_tmp = collect(1:orbitalNums[atom2]);
       for orbit2 in orbital_mask2
           deleteat!(orbital_mask2_tmp, find(orbital_mask2_tmp.==orbit2))
       end
-      Es_m_kq_up_atom2[orbitalStartIdx[atom2]+orbital_mask2_tmp,:]=0.0;
+      Es_m_kq_up_atom2[orbitalStartIdx_list[atom2]+orbital_mask2_tmp,:]   = 0.0;
+      Es_m_kq_down_atom2[orbitalStartIdx_list[atom2]+orbital_mask2_tmp,:]   = 0.0;
     end
 
     ## Do auctual calucations
-    Fftn_k::Array{Float_my,1}  = 1.0./(exp( ((En_k_down)  - ChemP)/(kBeV*E_temp)) + 1.0 );
-    Fftm_kq::Array{Float_my,1} = 1.0./(exp( ((Em_kq_up) - ChemP)/(kBeV*E_temp)) + 1.0 );
+    Fftn_k_up  = 1.0./(exp( ((En_k_up)  - ChemP)/(kBeV*E_temp)) + 1.0 );
+    Fftm_kq_up = 1.0./(exp( ((Em_kq_up) - ChemP)/(kBeV*E_temp)) + 1.0 );
 
-    dFnk_Fmkq::Array{Float_my,2} =
-      Fftn_k*ones(1,TotalOrbitalNum2)  - ones(TotalOrbitalNum2,1)*Fftm_kq[:]' ;
 
-    Enk_Emkq::Array{Complex_my,2} =
+    Fftn_k_down  = 1.0./(exp( ((En_k_down)  - ChemP)/(kBeV*E_temp)) + 1.0 );
+    Fftm_kq_down = 1.0./(exp( ((Em_kq_down) - ChemP)/(kBeV*E_temp)) + 1.0 );
+
+    dFnk_down_Fmkq_up =
+      Fftn_k_down*ones(1,TotalOrbitalNum2)  - ones(TotalOrbitalNum2,1)*Fftm_kq_up[:]' ;
+    dFnk_up_Fmkq_down =
+      Fftn_k_up*ones(1,TotalOrbitalNum2)  - ones(TotalOrbitalNum2,1)*Fftm_kq_down[:]' ;
+
+
+    Enk_down_Emkq_up =
       En_k_down[:]*ones(1,TotalOrbitalNum2) - ones(TotalOrbitalNum2,1)*Em_kq_up[:]' ;
+    Enk_up_Emkq_down =
+      En_k_up[:]*ones(1,TotalOrbitalNum2) - ones(TotalOrbitalNum2,1)*Em_kq_down[:]' ;
     #Enk_Emkq += im*0.001;
 
-    V1 = 0.5 *(Hks_up[atom1_orbitals,atom1_orbitals] - Hks_down[atom1_orbitals,atom1_orbitals])
-    V2 = 0.5 *(Hks_up[atom2_orbitals,atom2_orbitals] - Hks_down[atom2_orbitals,atom2_orbitals])
+#=
+    V1_down_up = Hks_kq_up[atom1_orbitals,atom1_orbitals] - Hks_k_down[atom1_orbitals,atom1_orbitals];
+    V2_up_down = Hks_k_down[atom2_orbitals,atom2_orbitals] - Hks_kq_up[atom2_orbitals,atom2_orbitals];
 
-    atom1_orbitals_rel = 1:orbitalNums[atom1];
-    atom2_orbitals_rel = 1:orbitalNums[atom2];
-    atom2_orbitals_rel2 = orbitalNums[atom1]+atom2_orbitals_rel;
+    V1_down_up *= 0.5;
+    V2_up_down *= 0.5;
 
-    #V1_2 = 0.5 *( SmallHks[1][atom1_orbitals_rel,atom1_orbitals_rel] -
-    #  SmallHks[2][atom1_orbitals_rel,atom1_orbitals_rel] );
-    #V2_2 = 0.5 *( SmallHks[1][atom2_orbitals_rel2,atom2_orbitals_rel2] -
-    #   SmallHks[2][atom2_orbitals_rel2,atom2_orbitals_rel2] );
+    V1_up_down   = Hks_kq_down[atom1_orbitals,atom1_orbitals] - Hks_k_up[atom1_orbitals,atom1_orbitals];
+    V2_down_up = Hks_k_up[atom2_orbitals,atom2_orbitals] - Hks_kq_down[atom2_orbitals,atom2_orbitals];
 
-    VV1::Array{Complex_my,2} = (Es_n_k_down_atom1[atom1_orbitals,:]' * V1 * Es_m_kq_up_atom1[atom1_orbitals,:]);
-    VV2::Array{Complex_my,2} = (Es_m_kq_up_atom2[atom2_orbitals,:]' * V2 * Es_n_k_down_atom2[atom2_orbitals,:]);
+    V1_up_down *= 0.5;
+    V2_down_up *= 0.5;
+=#
+    V1_G = Hks_G_up[atom1_orbitals,atom1_orbitals] - Hks_G_down[atom1_orbitals,atom1_orbitals];
+    V2_G = Hks_G_up[atom2_orbitals,atom2_orbitals] - Hks_G_down[atom2_orbitals,atom2_orbitals];
+    V1_G *= 0.5;
+    V2_G *= 0.5;
 
-    #VV1::Array{Complex_my,2} = (Es_n_k_down_atom1[atom1_orbitals,:]' * Es_m_kq_up_atom1[atom1_orbitals,:]);
-    #VV2::Array{Complex_my,2} = (Es_m_kq_up_atom2[atom2_orbitals,:]'  * Es_n_k_down_atom2[atom2_orbitals,:]);
+    V1_q = 0.5*(Hks_q_up[atom1_orbitals,atom1_orbitals]-Hks_q_down[atom1_orbitals,atom1_orbitals])
+    V2_q = 0.5*(Hks_q_up[atom2_orbitals,atom2_orbitals]-Hks_q_down[atom2_orbitals,atom2_orbitals])
 
-    Vi_Vj = transpose(VV1).*VV2;
+    V1_q_n = 0.5*(Hks_q_n_up[atom1_orbitals,atom1_orbitals]-Hks_q_n_down[atom1_orbitals,atom1_orbitals])
+    V2_q_n = 0.5*(Hks_q_n_up[atom2_orbitals,atom2_orbitals]-Hks_q_n_down[atom2_orbitals,atom2_orbitals])
 
-    J_ij::Array{Complex_my,2} =  0.5./(-Enk_Emkq).*dFnk_Fmkq .* Vi_Vj ;
+
+    V1_k = 0.5*(Hks_k_up[atom1_orbitals,atom1_orbitals] - Hks_k_down[atom1_orbitals,atom1_orbitals]);
+    V2_kq = 0.5*(Hks_kq_up[atom2_orbitals,atom2_orbitals] - Hks_kq_down[atom2_orbitals,atom2_orbitals]);
+
+    V1_kq = 0.5*(Hks_kq_up[atom1_orbitals,atom1_orbitals] - Hks_kq_down[atom1_orbitals,atom1_orbitals]);
+    V2_k = 0.5*(Hks_k_up[atom2_orbitals,atom2_orbitals] - Hks_k_down[atom2_orbitals,atom2_orbitals]);
+
+    V1_kq_k = 0.5*(Hks_kq_up[atom1_orbitals,atom1_orbitals] - Hks_k_down[atom1_orbitals,atom1_orbitals]);
+    V2_kq_k = 0.5*(Hks_kq_up[atom2_orbitals,atom2_orbitals] - Hks_k_down[atom2_orbitals,atom2_orbitals]);
+    #atom1_orbitals_rel = 1:orbitalNums[atom1];
+    #atom2_orbitals_rel = 1:orbitalNums[atom2];
+    #atom2_orbitals_rel2 = orbitalNums[atom1]+atom2_orbitals_rel;
+
+    VV1_down_up_G = Es_n_k_down_atom1[atom1_orbitals,:]' * V1_G *
+            Es_m_kq_up_atom1[atom1_orbitals,:];
+    VV2_up_down_G = Es_m_kq_up_atom2[atom2_orbitals,:]' * V2_G *
+        Es_n_k_down_atom2[atom2_orbitals,:];
+
+
+    VV1_down_up = Es_n_k_down_atom1[atom1_orbitals,:]' * V1_kq *
+            Es_m_kq_up_atom1[atom1_orbitals,:];
+    VV2_up_down = Es_m_kq_up_atom2[atom2_orbitals,:]' * V2_k *
+        Es_n_k_down_atom2[atom2_orbitals,:];
+
+    VV1_up_down = Es_n_k_up_atom1[atom1_orbitals,:]' * V1_kq *
+            Es_m_kq_down_atom1[atom1_orbitals,:];
+    VV2_down_up = Es_m_kq_down_atom2[atom2_orbitals,:]' * V2_k *
+        Es_n_k_up_atom2[atom2_orbitals,:];
+
+    Vi_Vj_down_up_up_down_G = transpose(VV1_down_up_G).*VV2_up_down_G;
+    Vi_Vj_down_up_up_down = transpose(VV1_down_up).*VV2_up_down;
+    Vi_Vj_up_down_down_up = transpose(VV1_up_down).*VV2_down_up;
+
+    J_ij_G =  0.5./(-Enk_down_Emkq_up).*dFnk_down_Fmkq_up .* Vi_Vj_down_up_up_down_G ;
+    J_ij_1 =  0.5./(-Enk_down_Emkq_up).*dFnk_down_Fmkq_up .* Vi_Vj_down_up_up_down ;
+    J_ij_2 =  0.5./(-Enk_up_Emkq_down).*dFnk_up_Fmkq_down .* Vi_Vj_up_down_down_up ;
+
     #return sum(J_ij[:])*Hartree2cm;
     #return sum(J_ij[!isnan(J_ij)] )*Hartree2cm;
-    result_mat[1,atom12_i] = sum(J_ij[!isnan(J_ij)] );
+
+    result_mat[1,atom12_i] = sum(J_ij_G[!isnan(J_ij_G)] );
+    result_mat[2,atom12_i] = sum(J_ij_1[!isnan(J_ij_1)] ) + sum(J_ij_2[!isnan(J_ij_2)] );
+    result_mat[3,atom12_i] = sum(J_ij_1[!isnan(J_ij_1)] );
+    result_mat[4,atom12_i] = sum(J_ij_2[!isnan(J_ij_2)] );
   end
   return result_mat
 end
 
-num_return = 1;
+num_return = 4;
 
 ## 4.1 Do K,Q sum
 # for orbital_mask1_list,orbital_mask2_list combinations
@@ -340,7 +434,7 @@ for (orbital1_i,orbital_mask1) in enumerate(orbital_mask1_list)
     optionalOutputDict["num_return"] = num_return;
     optionalOutputDict["VERSION_Spin_Exchange"] = string(X_VERSION);
 
-    export2mat_K_Q(Xij_Q_mean_matlab,scf_r,q_point_list,k_point_list,atom12_list,
+    export2mat_K_Q(Xij_Q_mean_matlab,hamiltonian_info,q_point_list,k_point_list,atom12_list,
     orbital_mask_on,orbital_mask1,orbital_mask2,ChemP_delta_ev,
     optionalOutputDict,
     jq_output_dir,cal_name,
