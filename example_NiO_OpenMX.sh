c428391436bc0212456191306fd96cd85dd97cb4
bluehope <bluehope@gmail.com>
2018-08-06 01:52:38 +0900

    Updated example scripte realated wiht 2ef5bce85c04f0261c6c65c10b9bdfc720068e66

--- example_NiO_OpenMX.sh
+++ example_NiO_OpenMX.sh
@@ -6,7 +6,7 @@ cd "examples/NiO_G-AFM.OpenMx"
 # nio.HWR : Wannier Hamiltonian from OpenMX
 tar xvf nio_dft_result.tar.xz
 # obtain J(q)
-julia -p 4 ~/.julia/v0.6/DFTforge/src/Spin_Exchange.jl  -T nio_J_openmx.toml
+julia -p 4 ~/.julia/v0.6/DFTforge/src/Jx_col_spin_exchange.jl  -T nio_J_openmx.toml
 # J(q) -> J(R)
 julia ~/.julia/v0.6/DFTforge/src/Jx_postprocess.jl --cellvectors  3_3_3 --atom12 1_1,1_2  --orbital_name all_all --baseatom 1 jq.col.spin_0.0
 
