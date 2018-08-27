b13b593655a9272bb86685363f58dabf4acfb143
bluehope <bluehope@gmail.com>
2018-08-27 14:08:21 +0900

    #7 Testing for Julia 0.7 : Warning removing.

--- src/basisTransform.jl
+++ src/basisTransform.jl
@@ -151,9 +151,10 @@ function rot_basis!(mat_original,orbitalStartIdx::Array{Int},
     @assert(Array{Float64,2} == typeof(orbital_rot));
     for duplicated_orbital_i in 1:duplicated_orbital_num
       rel_orbital_list = map(x->x[duplicated_orbital_i], v.orbital_list);
-      atom_orbitals = orbitalStartIdx[atom1] + rel_orbital_list
+      atom_orbitals = orbitalStartIdx[atom1] .+ rel_orbital_list
       # gen orbital_rot_full
       orbital_rot_full = eye(size(mat_original)[1]);
+	  orbital_rot_full = Matrix(1.0I, m, m);
       orbital_rot_full[atom_orbitals,atom_orbitals] = orbital_rot
       # actual rotation
       # Aroted = U * A * A'
