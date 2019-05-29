bd7a459eeab35c2153b6107c40bbc1789dad1b77
bluehope <bluehope@gmail.com>
2019-05-30 05:34:59 +0900

    Only msg update.

--- example_NiO_OpenMX.sh
+++ example_NiO_OpenMX.sh
@@ -20,13 +20,13 @@ tar xvf nio_dft_result.tar.xz
 cd "../../"
 # obtain J(q)
 printf "${BLUE} 1. Calculate J(q) ${NC} \n"
-printf "${GREEN}   julia -p 4 src/Jx_col_spin_exchange.jl  -T examples/NiO_G-AFM_U0.OpenMx/nio_J_openmx.toml ${NC}"
-printf "${GREEN}   julia --machine-file <file>     Run processes on hosts listed in <file> is also possible ${NC}"
+printf "${GREEN}   julia -p 4 src/Jx_col_spin_exchange.jl  -T examples/NiO_G-AFM_U0.OpenMx/nio_J_openmx.toml ${NC} \n"
+printf "${GREEN}   'julia --machine-file <file>' instead of 'julia -p 4' is also possible ${NC} \n"
 sleep 2
 #julia -p 4 src/Jx_col_spin_exchange.jl  -T examples/NiO_G-AFM.OpenMx/nio_J_openmx.toml
 julia -p 4 src/Jx_col_spin_exchange.jl  -T examples/NiO_G-AFM_U0.OpenMx/nio_J_openmx.toml
 # J(q) -> J(R)
 printf "${BLUE} 2. Transform J(q) -> J(Rij) ${NC} \n"
-printf "${GREEN}   julia  src/Jx_postprocess.jl --cellvectors  2_2_2 --baseatom1 1 --atom2 1,2 --orbital_name all_all  examples/NiO_G-AFM_U0.OpenMx/jx.col.spin_0.0 ${NC}"
+printf "${GREEN}   julia  src/Jx_postprocess.jl --cellvectors  2_2_2 --baseatom1 1 --atom2 1,2 --orbital_name all_all  examples/NiO_G-AFM_U0.OpenMx/jx.col.spin_0.0 ${NC} \n"
 sleep 2
 julia  src/Jx_postprocess.jl --cellvectors  2_2_2 --baseatom1 1 --atom2 1,2 --orbital_name all_all  examples/NiO_G-AFM_U0.OpenMx/jx.col.spin_0.0
